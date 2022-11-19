6472ae9c69797ce4f6fb3126572e6fd54d73abda
Ankit Verma <imankit.v5@gmail.com>
2022-11-19 08:13:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import {Navbar} from './Navbar';
+import "./App.css";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <div className="">
-      <Navbar />
+      <Navbar /> <Navbar /> <Navbar /> <Navbar />
       <h1>this is to test the rebase</h1>
       <p>this is a new commit</p>
       <div>rakesh changes</div>
       <p>ankit changes</p>
-        <p>ankit changess 2</p>
+      <p>ankit changess 2</p>
+      <h1>this is to test the rebase</h1>
+      <p>this is a new commit</p>
+      <div>rakesh changes</div>
+      <p>ankit changes</p>
+      <p>ankit changess 2</p>
+      <h1>this is to test the rebase</h1>
+      <p>this is a new commit</p>
+      <div>rakesh changes</div>
+      <p>ankit changes</p>
+      <p>ankit changess 2</p>
+      <h1>this is to test the rebase</h1>
+      <p>this is a new commit</p>
+      <div>rakesh changes</div>
+      <p>ankit changes</p>
+      <p>ankit changess 2</p>
     </div>
   );
 }
